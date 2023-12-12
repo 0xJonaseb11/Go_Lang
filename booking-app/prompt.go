ddf1799b12ea505a0e6738ee2f08e562c8e3034d
jonas-sebera <sebejaz99@gmail.com>
2023-12-12 11:08:42 +0200

    update

--- booking-app/prompt.go
+++ booking-app/prompt.go
@@ -5,11 +5,15 @@ import (
 	"bufio"
 )
 
-func main () {
+func prompt () {
 	fmt.Print("Enter your name: ");
 
 	// Read user input from the console
 	scanner := bufio.NewScanner(os.Stdin);
 	scanner.Scan();
 	name := scanner.Text();
+
+	// Print a personalized greeting
+	fmt.Printf("Hello, %s! Welcome to the world Of Go Programming.\n", name);
+
 }
