48cd2033a1b9fa3f17853431ba1710583393c63e
Jaz-3-0 <sebejaz99@gmail.com>
2024-04-07 02:30:10 +0200

    update switch, slices && init maps

--- go_book/helloWorld.go
+++ go_book/helloWorld.go
@@ -97,9 +97,105 @@ func main () {
 		fmt.Println("Two");
 	} else if nbr == 3 {
 		fmt.Println("Three");
+	} else if i == 4 {
+		fmt.Println("Four")
+	} else if i == 5 {
+		fmt.Println("Five")
 	} else {
 		fmt.Println("Invalid input")
 	}
+
+	// Opt to use switch case instead
+	switch i {
+	  case 0: fmt.Println("Zero");
+	  case 1: fmt.Println("One");
+	  case 2: fmt.Println("Two");
+	  case 3: fmt.Println("Three");
+	  case 4: fmt.Println("Four");
+	  case 5: fmt.Println("Five");
+	default: fmt.Println("Invalid input");
+	}
+
+	// Arrays 
+	// syntx - 
+	var array[5] int;
+	array[4] = 100;
+	fmt.Println(array);
+
+	var marks[5] float64
+	marks[0] = 98;
+	marks[1] = 45;
+	marks[2] = 76;
+	marks[3] = 89;
+	marks[4] = 100;
+
+	// also shortens the array - shorter syntax
+	_marks := [7] float64 { 65,87,98,77,99,55,33 }
+	fmt.Println(_marks);
+
+	var total float64 = 0;
+
+	for i := 0; i < 5; i++ {
+		total += marks[i];
+	}
+	fmt.Println(total / 5)
+
+	/*  To encounter for every change that can occur,
+	* we need to use the actual length of the array 
+	* rather than hard coding it
+    
+	*/
+
+	for i := 0; i < len(marks); i++ {
+		total += marks[i];
+	}
+	fmt.Println(total / float64(len(marks)));
+
+	// use a specail form of for loop
+	for _/*means we don't need this iterator variable*/, value := range marks {
+		total += value;
+	}
+	fmt.Println(total / float64(len(marks)));
+
+	// slices - segment of an array -- dynamic array
+	var slice [] float64 
+	fmt.Println("sample slice", slice);
+	// create a slice - use built-in make() function
+	slice_ := make([] float64, 5, 10);
+	fmt.Println(slice_)
+	// where 5 is the length of the array and 10 is the capacity of the underlying array which the slice points to.
+
+	/*
+	* Another way to create slices is the use of [low:high] expression
+	*/
+	arr := [9] float64 {1,2,3,4,5,6,7,8,9}
+	sliced := arr[4:8]
+	sliced_len := arr[0:len(arr)]
+	fmt.Println(sliced);
+	fmt.Println(sliced_len); // for convenience
+
+	// slice functions
+	// append()
+	slice1 := []int{1,2,3}
+	slice2 := append(slice1,4,5,6);
+	fmt.Println("Before appending: ", slice1 , "After appending:", slice2 );
+
+	// copy - copies content of slice1 into slice2
+	slice1_ := []int{1,2,3}
+	slice2_ := make([]int, 3)
+	copy(slice2_, slice1_)
+	fmt.Println("Before copying slice1 into slice2:", slice1_, "After copying slice1", slice2_);
+
+	// Maps
+	var map map[string]int
+
+
+	
+	
+	
+
+
+
 
 
 }
